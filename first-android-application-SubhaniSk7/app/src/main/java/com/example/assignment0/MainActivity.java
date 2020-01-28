89ddba6c016797c46d6217df38fe7c311367e3c8
SubhaniSk7 <subhanissm007@gmail.com>
2020-01-28 11:31:54 +0530

    updated

---
+++ first-android-application-SubhaniSk7/app/src/main/java/com/example/assignment0/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.assignment0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent activity2Intent = new Intent(this, Main2Activity.class);
+        startActivity(activity2Intent);
+    }
+}
